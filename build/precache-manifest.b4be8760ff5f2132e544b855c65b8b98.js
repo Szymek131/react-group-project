2fcfc688a49c3310799000f11f54b8ea79ec3dc6
Szymek131 <sz.wiktor131@gmail.com>
2023-04-13 22:12:38 +0200

    built project

---
+++ build/precache-manifest.b4be8760ff5f2132e544b855c65b8b98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd10d0b2e40d26f067e8a72d325f087a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf12dc53c70644f6089e",
+    "url": "/static/css/2.dc660faf.chunk.css"
+  },
+  {
+    "revision": "98777e8c0424b0e73703",
+    "url": "/static/css/main.17220207.chunk.css"
+  },
+  {
+    "revision": "bf12dc53c70644f6089e",
+    "url": "/static/js/2.5686cfef.chunk.js"
+  },
+  {
+    "revision": "98777e8c0424b0e73703",
+    "url": "/static/js/main.be7414d1.chunk.js"
+  },
+  {
+    "revision": "3d5b1b60935895d7db2c",
+    "url": "/static/js/runtime-main.efc0c886.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
